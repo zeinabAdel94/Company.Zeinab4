2f8d23b9efa87bef9a981efa3d4dc1bd3fc186e3
DELL <DELL@DESKTOP-8H1C7SH>
2025-03-12 02:51:20 +0200

    Implementation Of Employee DAL

---
+++ Company.Zeinab4.DAL/Data/Configrations/EmployeeConfgiration.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Company.Zeinab4.DAL.Modules;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore.Metadata.Conventions;
+
+namespace Company.Zeinab4.DAL.Data.Configrations
+{
+    internal class EmployeeConfgiration : IEntityTypeConfiguration<Employee>
+    {
+        public void Configure(EntityTypeBuilder<Employee> builder)
+        {
+            builder.Property(E => E.Salary).HasColumnType("decimal(18, 2)");
+        }
+    }
+}
